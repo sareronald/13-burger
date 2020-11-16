7c45ed33e940ad1c0732c5de172c65fc00af5e29
Sarah Ronald <sareronald@hotmail.com>
2020-11-16 15:07:26 +1100

    setting up JawsDB provision for Heroku

--- config/connection.js
+++ config/connection.js
@@ -4,14 +4,22 @@ const mysql = require("mysql");
 //Read and set any environment variables with the dotenv package.
 require("dotenv").config();
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: process.env.MYSQL_PASSWORD,
-  database: "burgers_db",
-});
+var connection;
 
+//Define database connection properties (host, user, password, and database name)
+//Use production database when deployed.
+if (process.env.JAWSDB_URL) {
+  // Heroku deployment
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: process.env.MYSQL_PASSWORD,
+    database: "burgers_db",
+  });
+}
 // Make connection.
 connection.connect(function (err) {
   if (err) {
